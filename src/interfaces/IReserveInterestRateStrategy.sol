2cdfb564e1193e36c7a9a3cd95a42d441555cb9e
yoghantara08 <gyoghantara@gmail.com>
2025-04-22 14:56:42 +0800

    add LendingPoolStorage and ReserveLogic library with interest rate calculations

---
+++ src/interfaces/IReserveInterestRateStrategy.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.28;
+
+interface IReserveInterestRateStrategy {
+    function baseVariableBorrowRate() external view returns (uint256);
+
+    function getMaxVariableBorrowRate() external view returns (uint256);
+
+    function calculateInterestRates(
+        address reserve,
+        uint256 availableLiquidity,
+        uint256 totalStableDebt,
+        uint256 totalVariableDebt,
+        uint256 averageStableBorrowRate,
+        uint256 reserveFactor
+    ) external view returns (uint256, uint256, uint256);
+
+    function calculateInterestRates(
+        address reserve,
+        address aToken,
+        uint256 liquidityAdded,
+        uint256 liquidityTaken,
+        uint256 totalStableDebt,
+        uint256 totalVariableDebt,
+        uint256 averageStableBorrowRate,
+        uint256 reserveFactor
+    )
+        external
+        view
+        returns (uint256 liquidityRate, uint256 stableBorrowRate, uint256 variableBorrowRate);
+}
