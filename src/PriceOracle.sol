b7e3c6d6d0d672fc7d0e69ee44e8dab4928c8a12
yoghantara08 <gyoghantara@gmail.com>
2025-04-28 16:09:00 +0800

    add asset price retrieval functions to PriceOracle contract

--- src/PriceOracle.sol
+++ src/PriceOracle.sol
@@ -4,7 +4,27 @@ pragma solidity ^0.8.28;
 import "@redstone-finance/evm-connector/contracts/data-services/RapidDemoConsumerBase.sol";
 
 contract PriceOracle is RapidDemoConsumerBase {
+    mapping(address => bytes32) public assetToDataFeedId;
+
     function getLatestPrice(bytes32 dataFeedId) public view returns (uint256) {
         return getOracleNumericValueFromTxMsg(dataFeedId);
+    }
+
+    function setAssetSource(address asset, bytes32 dataFeedId) external {
+        assetToDataFeedId[asset] = dataFeedId;
+    }
+
+    function getAssetPrice(address asset) public view virtual returns (uint256) {
+        bytes32 feedId = assetToDataFeedId[asset];
+        require(feedId != bytes32(0), "No data feed for asset");
+        return getOracleNumericValueFromTxMsg(feedId);
+    }
+
+    function getAssetsPrices(address[] calldata assets) external view returns (uint256[] memory) {
+        uint256[] memory prices = new uint256[](assets.length);
+        for (uint256 i = 0; i < assets.length; i++) {
+            prices[i] = getAssetPrice(assets[i]);
+        }
+        return prices;
     }
 }
