d598997ff5020826d042e0ab9a6d6fb3d00aca52
yoghantara08 <gyoghantara@gmail.com>
2025-04-29 22:39:44 +0800

    lending pool core contract deploy script

---
+++ script/LendingPool.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Script, console} from "../lib/forge-std/src/Script.sol";
+import {LendingPoolAddressesProvider} from "../src/configuration/LendingPoolAddressesProvider.sol";
+import {LendingPoolConfigurator} from "../src/LendingPoolConfigurator.sol";
+import {LendingPoolCollateralManager} from "../src/LendingPoolCollateralManager.sol";
+import {LendingPool} from "../src/LendingPool.sol";
+
+contract DeployLendingPool is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 emergencyAdminPrivateKey = vm.envUint("EMERGENCY_ADMIN_PRIVATE_KEY");
+
+        address deployer = vm.addr(deployerPrivateKey);
+        address emergencyAdmin = vm.addr(emergencyAdminPrivateKey);
+
+        string memory marketId = "KronoLiskSepolia";
+
+        // Start Deployments
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy Core Contracts
+        // 1. LendingPoolAddressesProvider
+        // 2. LendingPoolConfigurator
+        // 3. LendingPoolCollateralManager
+        // 4. LendingPool
+        LendingPoolAddressesProvider addressesProvider = new LendingPoolAddressesProvider(marketId);
+        LendingPoolConfigurator configurator = new LendingPoolConfigurator();
+        LendingPoolCollateralManager collateralManager = new LendingPoolCollateralManager();
+        LendingPool lendingPool = new LendingPool();
+
+        // Set Implementations
+        // This include initializing the contract implementation by LendingPoolAddressesProvider
+        addressesProvider.setLendingPoolConfiguratorImpl(address(configurator));
+        addressesProvider.setLendingPoolCollateralManager(address(collateralManager));
+        addressesProvider.setLendingPoolImpl(address(lendingPool));
+
+        // Set Admins
+        addressesProvider.setPoolAdmin(deployer);
+        addressesProvider.setEmergencyAdmin(emergencyAdmin);
+
+        // Log deployed contract addresses
+        console.log("LendingPoolAddressesProvider:", address(addressesProvider));
+        console.log("LendingPoolConfiguratorImpl:", address(configurator));
+        console.log("LendingPoolCollateralManagerImpl:", address(collateralManager));
+        console.log("LendingPoolImpl:", address(lendingPool));
+        console.log("LendingPoolConfiguratorProxy:", addressesProvider.getLendingPoolConfigurator()); // prettier-ignore
+        console.log("LendingPoolCollateralManagerProxy:", addressesProvider.getLendingPoolCollateralManager()); // prettier-ignore
+        console.log("LendingPoolProxy:", addressesProvider.getLendingPool());
+
+        vm.stopBroadcast();
+    }
+}
