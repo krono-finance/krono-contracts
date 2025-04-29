1cd534d5a2ecdb7f1fb6ce9077c76a653f74f6ab
yoghantara08 <gyoghantara@gmail.com>
2025-04-29 18:45:26 +0800

    refactor MockERC20 and MockPriceOracle

--- src/mocks/MockERC20.sol
+++ src/mocks/MockERC20.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.28;
 import {ERC20} from "../dependencies/openzeppelin/contracts/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
+        _setupDecimals(decimals_);
+    }
 
     function mint(address account, uint amount) external {
         _mint(account, amount);
--- src/mocks/MockPriceOracle.sol
+++ src/mocks/MockPriceOracle.sol
@@ -1,22 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "../PriceOracle.sol";
+contract MockPriceOracle {
+    mapping(address => uint256) private assetPrices;
 
-contract MockPriceOracle is PriceOracle {
-    uint256 private mockValue;
-
-    function setMockValue(uint256 _value) external {
-        mockValue = _value;
+    function setAssetPrice(address asset, uint256 value) external {
+        assetPrices[asset] = value;
     }
 
-    function getOracleNumericValueFromTxMsg(
-        bytes32 /*dataFeedId*/
-    ) internal view override returns (uint256) {
-        return mockValue;
-    }
-
-    function getAssetPrice(address asset) public view override returns (uint256) {
-        return mockValue;
+    function getAssetPrice(address asset) public view returns (uint256) {
+        return assetPrices[asset];
     }
 }
